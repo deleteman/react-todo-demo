5fd788fde7aa6fbe85537d9c9ba9c1b696e0f204
Fernando Doglio <fernando@azlo.com>
2020-01-31 00:32:16 +0100

    basic app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TodoList from './TodoList'
+import TodoItems from './TodoItems'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+    this.inputElement = React.createRef()
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+
+  addItem = (e) => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">      
+        <TodoList addItem={this.addItem}
+        inputElement={this.inputElement}
+        handleInput={this.handleInput}
+        currentItem={this.state.currentItem}
+        />
+        <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/TodoList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.props.addItem}>
+            <input placeholder="Task"
+            ref={this.props.inputElement}
+            value={this.props.currentItem.text}
+            onChange={this.props.handleInput}
+             />
+            <button type="submit"> Add Task </button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TodoList
